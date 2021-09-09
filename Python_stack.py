c6fb791fa9f885462b8a843a7ce9161285117b02
zubad <zubad.ali95@gmail.com>
2021-09-09 18:43:38 +0500

    Creating Stack in Python3

---
+++ Python_stack.py
@@ -0,0 +1,22 @@
+#Creating stack in Python
+
+stack = []
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+stack.append('e')
+stack.append('f')
+
+print (stack)
+
+var1  = stack.pop()
+var2  = stack.pop()
+var3  = stack.pop()
+
+print (var1)
+print (var2)
+print (var3)
+
+print (stack)
